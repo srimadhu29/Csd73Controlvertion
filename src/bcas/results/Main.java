ff46847cc1d3d6e6b83beb86b481c456d472269a
srimadhu29 <srimadushan.p@gmail.com>
2021-11-28 11:50:08 +0530

    Student marks updated

---
+++ src/bcas/results/Main.java
@@ -0,0 +1,18 @@
+package bcas.results;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String stno = "ST001";
+    int m1 = 55;
+    int m2 = 65;
+    int m3 = 90;
+    int total = m1+m2+m3;
+    int avg = total/3;
+        System.out.println("Marks 1 :"+m1);
+        System.out.println("Marks 2 :"+m2);
+        System.out.println("Marks 3 :"+m3);
+        System.out.println("Total :"+total);
+        System.out.println("Average :"+avg);
+    }
+}
